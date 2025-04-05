f01913f8ab35356305b647fd3a15f5c498d1952c
VictorHugoSS <146285315+VictorHugoSS@users.noreply.github.com>
2025-04-05 15:09:29 -0300

    Create app.py

---
+++ app.py
@@ -0,0 +1,30 @@
+import streamlit as st
+from datetime import date
+
+st.title("Checklist de Inspeção")
+
+colaborador = st.text_input("Colaborador")
+data = st.date_input("Data", value=date.today())
+
+st.markdown("### Itens de Verificação")
+check1 = st.checkbox("Equipamento limpo")
+check2 = st.checkbox("Sem vazamentos")
+check3 = st.checkbox("Sinalização adequada")
+check4 = st.checkbox("EPI utilizado corretamente")
+check5 = st.checkbox("Área isolada")
+
+observacoes = st.text_area("Observações")
+
+if st.button("Salvar"):
+    dados = {
+        "colaborador": colaborador,
+        "data": data.isoformat(),
+        "equipamento_limpo": check1,
+        "sem_vazamentos": check2,
+        "sinalizacao": check3,
+        "uso_epi": check4,
+        "area_isolada": check5,
+        "observacoes": observacoes
+    }
+    st.success("Checklist salvo com sucesso!")
+    st.write(dados)  # Aqui você pode substituir por envio ao SharePoint
